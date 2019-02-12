42c20c233f6309280cc03e0df7b0c80b81b92b76
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-12 04:57:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
  if array.length == 1
    return array.join
+ elsif array.length == 2
+   return array.join(" and ")
+ elsif array.length > 2
+   sentence = array
+   sentence.last = "and #{sentence.last}"
+   return sentence.join(", ")
  end
 end
