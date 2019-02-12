2a9eb29bc062d7f94a842af5bdefb8ab8878b06a
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-12 04:51:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,4 @@
 def oxford_comma(array)
-if array.length == 1
-  return array.join
-
-elsif array.length == 2
-  return array.join (" and ")
-
-elsif array.length == 3
-  array.insert(-1," and")
-  return array.join(, )
-
-end
-
+ if array.length == 1
+   return array.join
 end
